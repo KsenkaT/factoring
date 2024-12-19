8f12acec8f3270dec993da77f8c80dc378dc47f0
Kseniya Tarasevich <maudsava@gmail.com>
2024-12-19 16:16:39 +0300

    add come code

--- src/main/java/com/example/pastebin/PastebinApplication.java
+++ src/main/java/com/example/pastebin/PastebinApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PastebinApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //
         SpringApplication.run(PastebinApplication.class, args);
     }
 
